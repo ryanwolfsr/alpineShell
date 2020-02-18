a1fc4e823c730b1437caa8c46a8f45aec4c31699
Ryan Wolf <ryanwolfsr@gmail.com>
2020-02-17 22:49:50 -0500

    Testing

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 # Build images
-docker build -t ryanwolfsr/alpineShell:latest -t ryanwolfsr/alpineShell:$SHA -f ./alpineShell/Dockerfile ./alpineShell
+docker build -t ryanwolfsr/alpineshell:latest -t ryanwolfsr/alpineShell:$SHA -f ./alpineShell/Dockerfile ./alpineShell
 
 # Push to docker hub #SHA allows for easier updates. It essentially gives a "version" number for the image name in KUBE
-docker push ryanwolfsr/alpineShell:latest
-docker push ryanwolfsr/alpineShell:$SHA
+docker push ryanwolfsr/alpineshell:latest
+docker push ryanwolfsr/alpineshell:$SHA
 
 # Build kubernetes files
 kubectl apply -f k8sConfig
 
 # Set image deployment
-kubectl set image deployments/alpineShell-deployment alpineShell=ryanwolfsr/alpineShell:$SHA
+kubectl set image deployments/alpineshell-deployment alpineShell=ryanwolfsr/alpineshell:$SHA
